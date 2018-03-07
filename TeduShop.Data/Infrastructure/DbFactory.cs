c0454fe3399cb76e485471ec2f4ea9d39a0c932c
Dell-PC\Dell <buitienthanh1213@gmail.com>
2018-03-07 09:55:34 +0700

    add entity framwork code first for data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory:Disposable ,IDbFactory
+    {
+        TeduShopDbContext dbContect;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContect ?? (dbContect = new Data.TeduShopDbContext() );
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContect != null)
+                dbContect.Dispose();
+        }
+    }
+}
