ee2127f95863c39e5a71b64cb1db6ea5887c978d
Dell-PC\Dell <buitienthanh1213@gmail.com>
2018-03-07 11:29:22 +0700

    add more repositories for orther tables

--- TeduShop.Data/Repositories/ProductCategoryRepository.cs
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IProductCategoryRepository
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
     {
         IEnumerable<ProductCategory> GetByAlias(string alias);
     }
-    public class ProductCategoryRepository:RepositoryBase<ProductCategory>, IProductCategoryRepository
-    {
-        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
-        {
 
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
         }
 
         public IEnumerable<ProductCategory> GetByAlias(string alias)
