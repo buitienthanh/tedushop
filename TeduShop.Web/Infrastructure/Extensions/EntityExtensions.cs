e630edd082640744ec36d92ed3c313d9a553e4ec
Dell-PC\Dell <buitienthanh1213@gmail.com>
2018-04-05 09:39:08 +0700

    Quản lý Tags

--- TeduShop.Web/Infrastructure/Extensions/EntityExtensions.cs
+++ TeduShop.Web/Infrastructure/Extensions/EntityExtensions.cs
@@ -72,7 +72,7 @@ namespace TeduShop.Web.Infrastructure.Extensions
             product.MetaKeyword = productVm.MetaKeyword;
             product.MetaDescription = productVm.MetaDescription;
             product.Status = productVm.Status;
-
+            product.Tags = productVm.Tags;
         }
         public static void UpdatePost(this Post post, PostViewModel postVm)
         {
